69c7aee22177c5403e6bf7878b75b0da06161ae5
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2024-09-07 23:24:06 +0100

    project folder structure

---
+++ apps/main.cpp
@@ -0,0 +1,27 @@
+
+#include <boost/program_options.hpp>
+#include <iostream>
+
+
+#include "midas/version.h"
+
+namespace po = boost::program_options;
+
+int main(int argc, char *argv[]) {
+  po::options_description appOptionsDesc("Midas AlgoTrading options");
+  appOptionsDesc.add_options()("help,h", "prints this message")(
+      "version,v", "prints program version");
+
+  po::variables_map vm;
+  po::store(po::parse_command_line(argc, argv, appOptionsDesc), vm);
+
+  if (vm.empty() || vm.count("help")) {
+    std::cout << appOptionsDesc << '\n';
+  }
+
+  if (vm.count("version")) {
+    std::cout << "Midas version " << MIDAS_VERSION << '\n';
+  }
+
+  return 0;
+}
