2b205843283bb5cc51450c5afe41d5d483b9f008
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2024-09-17 01:09:46 +0100

    cmd line opts

--- apps/main.cpp
+++ apps/main.cpp
@@ -24,52 +24,20 @@ static po::options_description appOptionsDesc("Midas AlgoTrading options");
 
 static po::variables_map parseCmdLine(int argc, char *argv[]) {
   appOptionsDesc.add_options()("help,h", "prints this message")(
-      "version,v", "prints program version");
+      "version,v", "prints program version")(
+      "simulate", po::value<std::string>(),
+      "run simulation on historical file")("historical",
+                                           "download historical data");
   po::variables_map vm;
   po::store(po::parse_command_line(argc, argv, appOptionsDesc), vm);
   po::notify(vm);
   return vm;
 }
 
-int main(int argc, char *argv[]) {
-  logging::initialize_logging();
-  sigset_t sigset;
-  sigemptyset(&sigset);
-  // Only listen to interrupts and terminations
-  sigaddset(&sigset, SIGINT);
-  sigaddset(&sigset, SIGTERM);
-  pthread_sigmask(SIG_BLOCK, &sigset, nullptr);
-  std::atomic<bool> terminationRequested(false);
-  std::mutex signalHandlingCvMutex;
-  std::condition_variable signalHandlingCv;
-
-  boost::log::sources::severity_channel_logger_mt<logging::SeverityLevel,
-                                                  std::string>
-      lg(boost::log::keywords::channel = "cmdline",
-         boost::log::keywords::severity = logging::SeverityLevel::info);
-  LOG(lg, logging::SeverityLevel::info)
-      << "Midas v" << MIDAS_VERSION << " starting";
-  auto vm = parseCmdLine(argc, argv);
-  if (vm.empty() || vm.count("help")) {
-    std::cout << appOptionsDesc << '\n';
-  }
-  std::jthread signalHandler([&sigset, &terminationRequested,
-                              &signalHandlingCvMutex, &signalHandlingCv,
-                              &lg]() {
-    int signalNumber = 0;
-    timespec sigTimeout;
-    sigTimeout.tv_sec = 0;
-    sigTimeout.tv_nsec = 500000000;
-    do {
-      signalNumber = sigtimedwait(&sigset, nullptr, &sigTimeout);
-    } while (!terminationRequested.load() && signalNumber == -1 &&
-             errno == EAGAIN);
-    terminationRequested.store(true);
-    signalHandlingCv.notify_all();
-  });
-  if (vm.count("version")) {
-    std::cout << "Midas version v" << MIDAS_VERSION << '\n';
-  }
+static void downloadHistoricalData(std::mutex &signalHandlingCvMutex,
+                                   std::condition_variable &signalHandlingCv,
+                                   std::atomic<bool> &terminationRequested,
+                                   logging::thread_safe_logger_t &logger) {
   try {
     const auto address = boost::asio::ip::make_address("127.0.0.1");
     const auto endpoint = boost::asio::ip::tcp::endpoint(address, 7496);
@@ -120,7 +88,56 @@ int main(int argc, char *argv[]) {
     std::ofstream chartStream(filename, std::ios::out | std::ios::trunc);
     chartStream << mnqOneWeekChart << std::endl;
   } catch (NetworkError &e) {
-    std::cerr << "NETWORK error " << e.what() << std::endl;
+    ERROR_LOG(logger) << "NETWORK error " << e.what() << std::endl;
   }
+}
+
+static void runSimulation([[maybe_unused]] const std::string &filePath) {}
+
+int main(int argc, char *argv[]) {
+  logging::initialize_logging();
+  sigset_t sigset;
+  sigemptyset(&sigset);
+  // Only listen to interrupts and terminations
+  sigaddset(&sigset, SIGINT);
+  sigaddset(&sigset, SIGTERM);
+  pthread_sigmask(SIG_BLOCK, &sigset, nullptr);
+  std::atomic<bool> terminationRequested(false);
+  std::mutex signalHandlingCvMutex;
+  std::condition_variable signalHandlingCv;
+
+  auto logger = logging::create_channel_logger("cmdline");
+  INFO_LOG(logger) << "Midas v" << MIDAS_VERSION << " starting";
+  auto vm = parseCmdLine(argc, argv);
+
+  std::jthread signalHandler([&sigset, &terminationRequested,
+                              &signalHandlingCvMutex, &signalHandlingCv,
+                              &logger]() {
+    int signalNumber = 0;
+    timespec sigTimeout;
+    sigTimeout.tv_sec = 0;
+    sigTimeout.tv_nsec = 500000000;
+    do {
+      signalNumber = sigtimedwait(&sigset, nullptr, &sigTimeout);
+    } while (!terminationRequested.load() && signalNumber == -1 &&
+             errno == EAGAIN);
+    terminationRequested.store(true);
+    signalHandlingCv.notify_all();
+  });
+
+  if (vm.count("help")) {
+    std::cout << appOptionsDesc << '\n';
+  } else if (vm.count("version")) {
+    INFO_LOG(logger) << "Midas version v" << MIDAS_VERSION << '\n';
+  } else if (vm.count("historical")) {
+    downloadHistoricalData(signalHandlingCvMutex, signalHandlingCv,
+                           terminationRequested, logger);
+  } else if (vm.count("simulate")) {
+    runSimulation(vm["simulate"].as<std::string>());
+  } else {
+    ERROR_LOG(logger) << "No mode specified, use -h for options";
+  }
+  terminationRequested.store(true);
+  signalHandlingCv.notify_all();
   return 0;
 }
