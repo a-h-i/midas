0a6b1c5caf78460821913954c3d9d284ab5e0438
Ahmed H. Ismail <ahm3d.hisham@gmail.com>
2024-09-25 16:13:09 +0100

    downsampling in trader data

--- libs/trader/trader_data.cpp
+++ libs/trader/trader_data.cpp
@@ -1,10 +1,8 @@
 #include "exceptions/sampling_error.hpp"
 #include "trader/trader.hpp"
-#include <algorithm>
 #include <boost/circular_buffer.hpp>
-#include <cmath>
 #include <cstddef>
-#include <execution>
+#include <numeric>
 
 midas::trader::TraderData::TraderData(std::size_t lookBackSize,
                                       std::size_t candleSizeSeconds,
@@ -33,59 +31,19 @@ bool midas::trader::TraderData::ok() {
   // Depends on the vectors being kept at the same
   return tradeCounts.size() > lookBackSize;
 }
-template <typename T> class SampleWrapper {
-public:
-  class Iterator {
-  public:
-    using value_type = T *;
-    using difference_type = std::ptrdiff_t;
-    using pointer = T *;
-    using reference = value_type;
-    Iterator() : ptr(nullptr) {}
-    Iterator(pointer ptr) : ptr(ptr) {}
-    reference operator*() const { return ptr; }
-    pointer operator->() { return ptr; }
 
-    // Prefix increment
-    Iterator &operator++() {
-      ptr += 3;
-      return *this;
-    }
-
-    // Postfix increment
-    Iterator operator++(int) {
-      Iterator tmp = *this;
-      ++(*this);
-      return tmp;
-    }
-
-    friend bool operator==(const Iterator &a, const Iterator &b) {
-      return a.ptr == b.ptr;
-    };
-    friend bool operator!=(const Iterator &a, const Iterator &b) {
-      return a.ptr != b.ptr;
-    };
-
-  private:
-    pointer ptr;
-    SampleWrapper *wrapper;
-  };
-  SampleWrapper(Iterator::difference_type sampleWidth,
-                Iterator::difference_type numberOfSamples,
-                Iterator::pointer start)
-      : sampleWidth(sampleWidth), numberOfSamples(numberOfSamples),
-        start(start) {}
-
-  Iterator begin() { return Iterator(start); }
-  Iterator end() {
-    typename Iterator::difference_type offset = sampleWidth * numberOfSamples;
-    return Iterator(start + offset);
+template <typename Container, typename SourceContainer>
+void downSample(Container &destinationContainer,
+                const SourceContainer &sourceContainer,
+                std::ptrdiff_t downSampleRate, std::size_t &lastReadIndex) {
+  for (; lastReadIndex < sourceContainer.size();
+       lastReadIndex += downSampleRate) {
+    typename Container::value_type sum =
+        std::reduce(&sourceContainer[lastReadIndex],
+                    &sourceContainer[lastReadIndex + downSampleRate]);
+    destinationContainer.push_back(sum / downSampleRate);
   }
-
-private:
-  const Iterator::difference_type sampleWidth, numberOfSamples;
-  const Iterator::pointer start;
-};
+}
 
 void midas::trader::TraderData::clear() {
   std::scoped_lock lock(buffersMutex);
@@ -105,18 +63,17 @@ void midas::trader::TraderData::processSource() {
   if (!ok()) {
     return;
   }
-  const std::ptrdiff_t downSampleRate = candleSizeSeconds / source->barSizeSeconds;
+  const std::ptrdiff_t downSampleRate =
+      candleSizeSeconds / source->barSizeSeconds;
   const std::ptrdiff_t numCompleteSamples = source->size() / downSampleRate;
   if (numCompleteSamples == 0) {
     return;
   }
-  SampleWrapper<unsigned int> tradeCountsSample(
-      downSampleRate, numCompleteSamples, source->tradeCounts.data());
-  static_assert(std::forward_iterator<SampleWrapper<unsigned int>::Iterator>);
-
-  std::for_each(std::execution::par_unseq, tradeCountsSample.begin(),
-                tradeCountsSample.end(),
-                [this, downSampleRate]([[maybe_unused]]const unsigned int *range) {
-
-                });
+  downSample(tradeCounts, source->tradeCounts, downSampleRate, lastReadIndex);
+  downSample(highs, source->highs, downSampleRate, lastReadIndex);
+  downSample(lows, source->lows, downSampleRate, lastReadIndex);
+  downSample(opens, source->opens, downSampleRate, lastReadIndex);
+  downSample(closes, source->closes, downSampleRate, lastReadIndex);
+  downSample(waps, source->waps, downSampleRate, lastReadIndex);
+  downSample(volumes, source->volumes, downSampleRate, lastReadIndex);
 }
